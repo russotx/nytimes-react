f13ad36b7ea3a81256a35ddad992ff8196fe70c7
Daniel Russo <Daniel@Daniels-MacBook-Pro.local>
2017-05-22 12:25:44 -0500

    submit button working with no page refresh, form validation added for year input

--- src/components/search-pane.js
+++ src/components/search-pane.js
@@ -3,36 +3,85 @@ import React, { Component } from 'react';
 class SearchPane extends Component {
   constructor(props) {
     super(props)
-    this.state = {topic: '', startYear: '', endYear: ''};
+    this.state = {
+      topic: '', 
+      startYear: '', 
+      endYear: '', 
+      syValid: true, 
+      eyValid: true
+    };
     this.showText = this.showText.bind(this);
     this.fetchArticles = this.fetchArticles.bind(this);
+    this.isYearValid = this.isYearValid.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   showText(event){
     const name = event.target.name;
     this.setState({ [name]: event.target.value});
+    console.log(this.syValid);
   }
 
-  fetchArticles(event){
+
+  isYearValid(field, year) {
+    const validTag = field === "startYear" ? "syValid" : "eyValid";
+    if ((((parseInt(year)).toString()).length != 4) || (year.length > 4)) {
+        this.setState({ 
+          [field] : "please use YYYY format",
+          [validTag] : false
+        });
+        return false;
+    } else {
+      this.setState({
+        [validTag] : true
+      })
+      return true;
+    }
+  }
+
+  fetchArticles(topic,start,end) {
+
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
     const searchTopic = this.state.topic;
     const searchStart = this.state.startYear; 
     const searchEnd = this.state.endYear; 
-    console.log(searchTopic);
+    this.isYearValid("startYear",searchStart);
+    this.isYearValid("endYear",searchEnd);
+    
   }
 
   render(){
     return (
       <div className="comp-pane">
         <h3 className="title-box">Search</h3>
-        <form onSubmit={this.fetchArticles}>
+        <form onSubmit={this.handleSubmit}>
           <label>Topic
-          <input name="topic" type="text" value={this.state.topic} onChange={this.showText}/>
+          <input 
+            name="topic" 
+            type="text" 
+            value={this.state.topic} 
+            onChange={this.showText}/>
           </label>
           <label>Start Year
-          <input name="startYear" type="text" value={this.state.startYear} onChange={this.showText}/>
+          <input 
+            name="startYear" 
+            placeholder="YYYY"
+            className={this.state.syValid ? "valid-input" : "invalid-input"} 
+            type="number" 
+            value={this.state.startYear} 
+            onChange={this.showText}/>
           </label>
           <label>End Year
-          <input name="endYear" type="text" value={this.state.endYear} onChange={this.showText}/>
+          <input 
+            name="endYear" 
+            placeholder="YYYY"
+            className={this.state.eyValid ? "valid-input" : "invalid-input"} 
+            type="number" 
+            value={this.state.endYear} 
+            onChange={this.showText}/>
           </label>
           <input className="submit-button" name="Submit" type="submit" value="submit"/>
         </form>
